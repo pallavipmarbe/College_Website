3f57526fba90e2804495aa1133d65620370df3fb
pallavi marbe <pallavimarbe10@gmail.com>
2024-08-20 20:08:18 +0530

    b

--- App.js
+++ App.js
@@ -4,24 +4,4 @@
     font-family: 'Poppins', sans-serif;
 }
 .header {
-    height: 100vh;\
-    
-    width: 100%;
-    background-position: center;
-    background-size: cover;
-    position: relative;
-}
-
-@media (min-aspect-ratio:16/9) {
-    .back-video{
-        width: 100%;
-        height: auto;
-    }
-}
-@media (max-aspect-ratio:16/9) {
-    .back-video{
-        width: auto;
-        height: 100%;
-    }
-} 
-n
+ 
