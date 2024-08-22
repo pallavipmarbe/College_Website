1f56289083288d23cfde7de7a7d6bd0e3c2f9a02
pallavi marbe <pallavimarbe10@gmail.com>
2024-08-22 20:38:06 +0530

    q

--- App.js
+++ App.js
@@ -22,3 +22,24 @@
                 <li><a href="contact.html">CONTACT</a></li>
             </ul>
         </div>
+        <i class="fa fa-bars"  onclick="showMenu()"></i>
+    </nav>
+    <h1>Contact Us</h1>
+</section>
+
+<!-- Contact us -->
+
+<section class="location">
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3808.718030157608!2d76.8215208148755!3d17.329148688113495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc8b8aa170f760f%3A0x872fcb04853d307!2sSharnbasva%20University!5e0!3m2!1sen!2sin!4v1678618133511!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+</section>
+
+<section class="contact-us">
+
+    <div class="row">
+        <div class="contact-col">
+            <div>
+                <i class="fa fa-home"></i>
+                <span>
+                    <h5>Sharnbasva university, vidya nagar, kalburagi 585-103</h5>
