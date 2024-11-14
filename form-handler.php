1ed7a009aa2e48308f8d9914671790a893acb995
Pallavi Marbe <pallavimarbe10@gmail.com>
2024-11-14 19:49:54 +0530

    y

--- form-handler.php
+++ form-handler.php
@@ -38,3 +38,31 @@ header("Location: contact.html");
 
 
 
+name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'suk@gmail.com';
+
+$email_subject = 'new Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n";
+
+$to = 'laxmi.s.soddi@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?> 
+
+
+
